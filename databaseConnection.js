26d2c9f4b493ac796c2afd80a3eb877ce9aab0c3
byron dray <byrondray8@gmail.com>
2024-03-28 21:34:40 -0700

    new connection string

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,7 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const is_qoddi = process.env.IS_QODDI || false;
 const qoddiURI =
-  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongod b.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.rnp4igh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const localURI =
   "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 
