1f68b96175aabece42685ece831517739a9bf3ca
byron dray <byrondray8@gmail.com>
2024-03-28 19:22:42 -0700

    add connection string

--- databaseConnectionSequelize.js
+++ databaseConnectionSequelize.js
@@ -1,7 +1,7 @@
 const MongoClient = require("mongodb").MongoClient;
 const is_qoddi = process.env.IS_QODDI || false;
 const qoddiURI =
-  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.4ulcc.mongodb.net/myFirstDa tabase?retryWrites=true&w=majority";
+  "mongodb+srv://theMongoAdmin:accidentalLoginSteps@cluster0.rnp4igh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const localURI =
   "mongodb://127.0.0.1/?authSource=admin&retryWrites=true&w=majority";
 if (is_qoddi) {
